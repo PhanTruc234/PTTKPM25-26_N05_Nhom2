9813b03d249f2d253053fe37f2c64192a330a16a
PhanTruc234 <phantruc313@gmail.com>
2025-10-23 14:31:40 +0700

    cập nhật giỏ hàng

--- Source/FE/src/pages/account/payment/PaymentFailed.jsx
+++ Source/FE/src/pages/account/payment/PaymentFailed.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PaymentFailed = () => {
   return (
-    <div className="container p-20">
+    <div className="container p-20 mx-auto">
       <img src="https://cdn.9pay.vn/tin-tuc/ly-do-dan-den-thanh-toan-khong-thanh-cong-1694159534.jpg" alt="" />
     </div>
   )
